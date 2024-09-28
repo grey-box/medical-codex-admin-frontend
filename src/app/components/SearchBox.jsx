00f3610a7c9753f527417a20d53e094734b70436
Jnoce <jennanoce@gmail.com>
2024-09-28 18:36:18 -0400

    Minor UI improvements

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -23,13 +23,32 @@ function SearchBox() {
 
         #dropdown-content {
             display: none;
+            font-size: .75em;
+            color: black;
             list-style: none;
             max-height: 5em;
+            z-index: 2;
+            overflow-y: scroll;
             width: 100%;
+            background-color: var(--foreground);
+            border-radius: 0px 0px 1em 1em;
+        }
+        
+        #dropdown-content li {
+            padding: .5em 1em;
         }
 
         #dropdown-content li:hover {
-            background-color: blue;
+            background-color: var(--accent);
+            color: var(--foreground)
+        }
+
+        #search-box:focus + #dropdown-content{
+            display: block !important;
+        }
+
+        #search-box:focus {
+            border-radius: 1em 1em 0px 0px;
         }
     `;
     return (
