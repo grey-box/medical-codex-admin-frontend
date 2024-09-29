737ae26f0570f8cea598293f7a3609a6035134e6
MilesMurphy-greybox <miles.murphy@grey-box.ca>
2024-09-29 14:13:12 -0400

    fix: updated icon on navbar to correct path

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ function NavigationBar() {
           <Link href='/' className="text-[40px] font-black text-white no-underline">Greybox</Link>
         </div>
         <div className="flex items-center justify-center p-[10px] cursor-pointer" onClick={handleShowNavbar}>
-          <img src={`${process.env.PUBLIC_URL}/icon.png`} alt="Icon" />
+          <img src='/images/icons/icon.png' alt="Icon" />
         </div>
         <div className={`absolute right-0 top-[85px] p-[20px] bg-white border border-gray-300 rounded-md shadow-md z-1000 ${showNavbar ? 'block' : 'hidden'}`}>
           <ul className="flex flex-col list-none m-0 p-0">
