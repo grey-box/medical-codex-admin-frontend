1030e68a3ced4a88f577d233553e9300cde6ac87
Thayer Picart <thayer.picart@grey-box.ca>
2024-09-29 15:58:08 -0400

    refactor: convert CSS to inline Tailwind for about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,151 +1,152 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './About.css';
-import { FaTwitter, FaFacebook, FaGlobe, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import Link from "next/link";
+import { FaTwitter, FaFacebook, FaGlobe, FaLinkedin } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="about-page">
-      <div className="about-header">
-        <div className="image-container">
-        <div className="header-about-title">About Us</div>
-        <img src={`${process.env.PUBLIC_URL}/child.png`} className="child" alt="child in classroom" />
+    <div className="bg-[#B6E7FF]">
+      <div className="relative w-full h-auto">
+        <div className="relative w-full h-auto">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-extrabold text-center">
+            About Us
+          </div>
+          <img
+            src="/images/assets/child.png"
+            className="w-full h-auto block"
+            alt="child in classroom"
+          />
         </div>
       </div>
 
-      <div className='about-container'>
-        <h1 className='about-title'>Meet <br/> Our Team</h1>
-        <span className='rectangle'/>
-        <div className='team-section'>
+      <div className="flex flex-col mt-12 bg-[#B6E7FF]">
+        <div className="text-4xl font-extrabold mb-12 text-left pl-10 text-[#555555]">
+          Meet <br /> Our Team
+        </div>
 
-          <div className='team-member'>
-            <img src='../Abhishek.jpg' alt='team-member1' />
-            <h3 className="name">Abhishek Nair</h3>
-            <p className="job-title">Backend Developer, API Developer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/abhisheknair34005/' target="_blank"><FaLinkedin /></Link>
+        <div className="flex justify-center flex-wrap items-center">
+          {[
+            {
+              name: "Abhishek Nair",
+              title: "Backend Developer, API Developer",
+              img: "/images/people/Abhishek.jpg",
+              link: "https://www.linkedin.com/in/abhisheknair34005/",
+            },
+            {
+              name: "Sahil Jambhulkar",
+              title: "Backend Developer, Test Engineer",
+              img: "/images/people/Sahil.png",
+              link: "https://www.linkedin.com/in/sahil-jambhulkar/",
+            },
+            {
+              name: "Shyam Prasad",
+              title: "Web Designer, Frontend Developer",
+              img: "/images/people/Shyam.jpg",
+              link: "https://www.linkedin.com/in/shyam-prasad-yanamaddi-9213b8220/",
+            },
+            {
+              name: "Konark Bhad",
+              title: "Frontend Developer, Database Administrator",
+              img: "/images/people/KonarkBhad.jpg",
+              link: "https://www.linkedin.com/in/konark-bhad/",
+            },
+            {
+              name: "Bianca Scoropan",
+              title: "Volunteer Frontend Developer, Web Designer",
+              img: "/images/people/Bianca.png",
+              link: "https://www.linkedin.com/in/bianca-scoropan-576233220/",
+            },
+            {
+              name: "Ravneet Singh",
+              title: "Volunteer Backend Developer",
+              img: "/images/people/Ravneet.png",
+              link: "",
+            },
+            {
+              name: "Dominique Dupont-Jillings",
+              title: "Intern Backend Developer",
+              img: "/images/people/Dominique.png",
+              link: "https://www.linkedin.com/in/dominique-dupont-jillings-71303789/",
+            },
+            {
+              name: "Safwan Haque",
+              title: "Intern Backend Developer",
+              img: "/images/people/Safwan.png",
+              link: "https://www.linkedin.com/in/safwanhaque/",
+            },
+            {
+              name: "Arielle Kozin",
+              title: "Scrum Master Project Management",
+              img: "/images/people/Arielle.png",
+              link: "https://www.linkedin.com/in/rel-kozfam-362205113/",
+            },
+            {
+              name: "François Pelletier",
+              title: "Volunteer Data Scientist and Software Developer",
+              img: "/images/people/Francois.png",
+              link: "https://www.linkedin.com/in/francoispelletier-jevalideca/",
+            },
+          ].map((member, index) => (
+            <div
+              key={index}
+              className="w-[376px] h-[384px] m-5 p-5 bg-white shadow-md text-center transition-transform duration-300 ease-in-out rounded-2xl hover:translate-y-[-5px]"
+            >
+              <img
+                className="w-[349px] h-[195px] mb-5 object-cover"
+                src={member.img}
+                alt={`team-member${index + 1}`}
+              />
+              <h3 className="font-extrabold text-xl mb-2">{member.name}</h3>
+              <p className="font-light text-lg mb-5">{member.title}</p>
+              <div className="flex justify-center">
+                {member.link ? (
+                  <Link href={member.link} target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin className="text-blue-500 text-2xl mx-2 transition-colors duration-300 ease-in-out" />
+                  </Link>
+                ) : null}
+              </div>
             </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Sahil.png' alt='team-member2' />
-            <h3 className="name">Sahil Jambhulkar</h3>
-            <p className="job-title">Backend Developer, Test Engineer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/sahil-jambhulkar/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Shyam.jpg' alt='team-member3' />
-            <h3 className="name">Shyam Prasad</h3>
-            <p className="job-title">Web Designer, Frontend Developer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/shyam-prasad-yanamaddi-9213b8220/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../KonarkBhad.jpg' alt='team-member4' />
-            <h3 className="name">Konark Bhad</h3>
-            <p className="job-title">Frontend Developer, Database Administrator</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/konark-bhad/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          
-          <div className='team-member'>
-            <img src='../Bianca.png' alt='team-member4' />
-            <h3 className="name">Bianca Scoropan</h3>
-            <p className="job-title">Volunteer Frontend Developer, Web Designer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/bianca-scoropan-576233220/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Ravneet.png' alt='team-member4' />
-            <h3 className="name">Ravneet Singh</h3>
-            <p className="job-title">Volunteer Backend Developer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Dominique.png' alt='team-member4' />
-            <h3 className="name">Dominique Dupont-Jillings</h3>
-            <p className="job-title">Intern Backend Developer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/dominique-dupont-jillings-71303789/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Safwan.png' alt='team-member4' />
-            <h3 className="name">Safwan Haque</h3>
-            <p className="job-title">Intern Backend Developer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/safwanhaque/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Arielle.png' alt='team-member4' />
-            <h3 className="name">Arielle Kozin</h3>
-            <p className="job-title">Scrum Master Project Management</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/rel-kozfam-362205113/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
-          <div className='team-member'>
-            <img src='../Francois.png' alt='team-member4' />
-            <h3 className="name">François Pelletier</h3>
-            <p className="job-title">Volunteer Data Scientist and Software Developer</p>
-            <p>
-            </p>
-            <div className='social-links'>
-              <Link to='https://www.linkedin.com/in/francoispelletier-jevalideca/' target="_blank"><FaLinkedin /></Link>
-            </div>
-          </div>
-
+          ))}
         </div>
       </div>
 
-      <div className="footer">
-        <div className="icons">
-          <div className="social-media-icons">
-            <a href="https://www.grey-box.ca/" target="_blank"><FaGlobe /></a>
-            <a href="https://twitter.com/greyboxproject" target="_blank"><FaTwitter /></a>
-            <a href="https://www.facebook.com/greyboxORG/" target="_blank"><FaFacebook /></a>
-            <a href="https://www.linkedin.com/company/greyboxproject/" target="_blank"><FaLinkedin /></a>
-          </div>
+      <div className="relative bg-[#044677] p-5 text-center h-[60px] w-full">
+        <div className="flex justify-center">
+          <Link
+            href="https://www.grey-box.ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <FaGlobe />
+          </Link>
+          <Link
+            href="https://twitter.com/greyboxproject"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <FaTwitter />
+          </Link>
+          <Link
+            href="https://www.facebook.com/greyboxORG/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <FaFacebook />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/company/greyboxproject/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-2"
+          >
+            <FaLinkedin />
+          </Link>
         </div>
       </div>
-
     </div>
   );
-}
+};
 
 export default About;
-
