c0bde480e515b42d7b2e8890a536b58568b17741
Thayer Picart <thayer.picart@grey-box.ca>
2024-10-05 19:45:14 -0400

    refactor: converted About component from js to typescript

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,78 @@
-import React from "react";
-import Link from "next/link";
-import { FaTwitter, FaFacebook, FaGlobe, FaLinkedin } from "react-icons/fa";
+import React, { FC } from 'react';
+import Link from 'next/link';
+import { FaTwitter, FaFacebook, FaGlobe, FaLinkedin } from 'react-icons/fa';
 
-const About = () => {
+interface TeamMember {
+  name: string;
+  title: string;
+  img: string;
+  link: string;
+}
+
+const About: FC = () => {
+  const teamMembers: TeamMember[] = [
+    {
+      name: "Abhishek Nair",
+      title: "Backend Developer, API Developer",
+      img: "/images/people/Abhishek.jpg",
+      link: "https://www.linkedin.com/in/abhisheknair34005/",
+    },
+    {
+      name: "Sahil Jambhulkar",
+      title: "Backend Developer, Test Engineer",
+      img: "/images/people/Sahil.png",
+      link: "https://www.linkedin.com/in/sahil-jambhulkar/",
+    },
+    {
+      name: "Shyam Prasad",
+      title: "Web Designer, Frontend Developer",
+      img: "/images/people/Shyam.jpg",
+      link: "https://www.linkedin.com/in/shyam-prasad-yanamaddi-9213b8220/",
+    },
+    {
+      name: "Konark Bhad",
+      title: "Frontend Developer, Database Administrator",
+      img: "/images/people/KonarkBhad.jpg",
+      link: "https://www.linkedin.com/in/konark-bhad/",
+    },
+    {
+      name: "Bianca Scoropan",
+      title: "Volunteer Frontend Developer, Web Designer",
+      img: "/images/people/Bianca.png",
+      link: "https://www.linkedin.com/in/bianca-scoropan-576233220/",
+    },
+    {
+      name: "Ravneet Singh",
+      title: "Volunteer Backend Developer",
+      img: "/images/people/Ravneet.png",
+      link: "",
+    },
+    {
+      name: "Dominique Dupont-Jillings",
+      title: "Intern Backend Developer",
+      img: "/images/people/Dominique.png",
+      link: "https://www.linkedin.com/in/dominique-dupont-jillings-71303789/",
+    },
+    {
+      name: "Safwan Haque",
+      title: "Intern Backend Developer",
+      img: "/images/people/Safwan.png",
+      link: "https://www.linkedin.com/in/safwanhaque/",
+    },
+    {
+      name: "Arielle Kozin",
+      title: "Scrum Master Project Management",
+      img: "/images/people/Arielle.png",
+      link: "https://www.linkedin.com/in/rel-kozfam-362205113/",
+    },
+    {
+      name: "François Pelletier",
+      title: "Volunteer Data Scientist and Software Developer",
+      img: "/images/people/Francois.png",
+      link: "https://www.linkedin.com/in/francoispelletier-jevalideca/",
+    },
+  ];
+
   return (
     <div className="bg-[#B6E7FF]">
       <div className="relative w-full h-auto">
@@ -24,68 +94,7 @@ const About = () => {
         </div>
 
         <div className="flex justify-center flex-wrap items-center">
-          {[
-            {
-              name: "Abhishek Nair",
-              title: "Backend Developer, API Developer",
-              img: "/images/people/Abhishek.jpg",
-              link: "https://www.linkedin.com/in/abhisheknair34005/",
-            },
-            {
-              name: "Sahil Jambhulkar",
-              title: "Backend Developer, Test Engineer",
-              img: "/images/people/Sahil.png",
-              link: "https://www.linkedin.com/in/sahil-jambhulkar/",
-            },
-            {
-              name: "Shyam Prasad",
-              title: "Web Designer, Frontend Developer",
-              img: "/images/people/Shyam.jpg",
-              link: "https://www.linkedin.com/in/shyam-prasad-yanamaddi-9213b8220/",
-            },
-            {
-              name: "Konark Bhad",
-              title: "Frontend Developer, Database Administrator",
-              img: "/images/people/KonarkBhad.jpg",
-              link: "https://www.linkedin.com/in/konark-bhad/",
-            },
-            {
-              name: "Bianca Scoropan",
-              title: "Volunteer Frontend Developer, Web Designer",
-              img: "/images/people/Bianca.png",
-              link: "https://www.linkedin.com/in/bianca-scoropan-576233220/",
-            },
-            {
-              name: "Ravneet Singh",
-              title: "Volunteer Backend Developer",
-              img: "/images/people/Ravneet.png",
-              link: "",
-            },
-            {
-              name: "Dominique Dupont-Jillings",
-              title: "Intern Backend Developer",
-              img: "/images/people/Dominique.png",
-              link: "https://www.linkedin.com/in/dominique-dupont-jillings-71303789/",
-            },
-            {
-              name: "Safwan Haque",
-              title: "Intern Backend Developer",
-              img: "/images/people/Safwan.png",
-              link: "https://www.linkedin.com/in/safwanhaque/",
-            },
-            {
-              name: "Arielle Kozin",
-              title: "Scrum Master Project Management",
-              img: "/images/people/Arielle.png",
-              link: "https://www.linkedin.com/in/rel-kozfam-362205113/",
-            },
-            {
-              name: "François Pelletier",
-              title: "Volunteer Data Scientist and Software Developer",
-              img: "/images/people/Francois.png",
-              link: "https://www.linkedin.com/in/francoispelletier-jevalideca/",
-            },
-          ].map((member, index) => (
+          {teamMembers.map((member, index) => (
             <div
               key={index}
               className="w-[376px] h-[384px] m-5 p-5 bg-white shadow-md text-center transition-transform duration-300 ease-in-out rounded-2xl hover:translate-y-[-5px]"
