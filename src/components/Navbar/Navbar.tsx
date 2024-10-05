290c8b8c6b6ee1e71902335cf242bed2d717a6bb
Thayer Picart <thayer.picart@grey-box.ca>
2024-10-05 19:47:00 -0400

    refactor: converted Navbar component from js to typescript

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 import Link from 'next/link';
 
-function NavigationBar() {
-  const [showNavbar, setShowNavbar] = useState(false);
+const NavigationBar: FC = () => {
+  const [showNavbar, setShowNavbar] = useState<boolean>(false);
 
-  const handleShowNavbar = () => {
+  const handleShowNavbar = (): void => {
     setShowNavbar(!showNavbar);
   }
 
   return (
-    <div className="navbar">
-      <div className="flex justify-between items-center h-[100px] bg-[#0068B7] relative shadow-lg">
+    <div className="navbar relative">
+      <div className="flex justify-between items-center h-[100px] bg-[#0068B7] shadow-lg">
         <div className="flex items-center justify-center p-[10px]">
           <Link href='/' className="text-[40px] font-black text-white no-underline">Greybox</Link>
         </div>
         <div className="flex items-center justify-center p-[10px] cursor-pointer" onClick={handleShowNavbar}>
           <img src='/images/icons/icon.png' alt="Icon" />
         </div>
-        <div className={`absolute right-0 top-[85px] p-[20px] bg-white border border-gray-300 rounded-md shadow-md z-1000 ${showNavbar ? 'block' : 'hidden'}`}>
+        <div className={`absolute right-0 top-[85px] p-[20px] bg-white border border-gray-300 rounded-md shadow-md z-[1000] ${showNavbar ? 'block' : 'hidden'}`}>
           <ul className="flex flex-col list-none m-0 p-0">
             <li>
               <Link href="/about" className="block text-[16px] font-normal text-[#555555] no-underline p-[10px_20px]">About</Link>
