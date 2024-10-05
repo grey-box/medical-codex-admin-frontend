8cda2d1f687ac4b86de3b4fe108f713c62299bdc
Thayer Picart <thayer.picart@grey-box.ca>
2024-10-05 19:47:10 -0400

    refactor: converted Icons component from js to typescript

--- src/components/SocialMediaIcons/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons/SocialMediaIcons.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { FaTwitter, FaFacebook, FaGlobe, FaLinkedin } from "react-icons/fa";
+import React, { FC } from 'react';
+import { FaTwitter, FaFacebook, FaGlobe, FaLinkedin } from 'react-icons/fa';
 
-function SocialMediaIcons() {
+const SocialMediaIcons: FC = () => {
   return (
     <div className="flex items-center justify-center">
       <a
         href="https://www.grey-box.ca/"
         target="_blank"
+        rel="noopener noreferrer"
         className="mx-2 text-2xl text-white no-underline"
       >
         <FaGlobe />
@@ -14,6 +15,7 @@ function SocialMediaIcons() {
       <a
         href="https://twitter.com/greyboxproject"
         target="_blank"
+        rel="noopener noreferrer"
         className="mx-2 text-2xl text-white no-underline"
       >
         <FaTwitter />
@@ -21,6 +23,7 @@ function SocialMediaIcons() {
       <a
         href="https://www.facebook.com/greyboxORG/"
         target="_blank"
+        rel="noopener noreferrer"
         className="mx-2 text-2xl text-white no-underline"
       >
         <FaFacebook />
@@ -28,6 +31,7 @@ function SocialMediaIcons() {
       <a
         href="https://www.linkedin.com/company/greyboxproject/"
         target="_blank"
+        rel="noopener noreferrer"
         className="mx-2 text-2xl text-white no-underline"
       >
         <FaLinkedin />
