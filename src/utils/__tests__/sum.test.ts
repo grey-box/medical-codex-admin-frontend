c05edcbd2904e78ce1f364355539da6ed4118d8a
Matthew Quijano <mq022002@gmail.com>
2024-10-06 00:57:41 -0400

    docs: Add author, and test cases documentation to sum, like handleSearch

--- src/utils/__tests__/sum.test.ts
+++ src/utils/__tests__/sum.test.ts
@@ -1,8 +1,15 @@
+// Author: Jest [https://jestjs.io/docs/getting-started]
+
+// Notes:
 // This does not have any functionality within the application and is only used to confirm that Jest is working correctly.
 // To run tests, look at package.json. We have 3 scripts, but you want to run either:
 // 'npm run test' just to check if your tests pass (this takes a shorter amount of time to run)
 // 'npm run test:coverage' to check if your tests pass and to see the coverage of your tests (this takes a longer amount of time to run)
 // Both are equipped with 'watchAll' flag, so as you make changes, the tests kick off again automatically.
+
+// Test cases:
+// 1. It should add two numbers together.
+
 import sum from "../sum";
 
 test("adds 1 + 2 to equal 3", () => {
