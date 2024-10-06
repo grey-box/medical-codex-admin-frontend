8cbfcbbc617ed32621f57a5122a4d661ef650d93
Matthew Quijano <mq022002@gmail.com>
2024-10-05 21:23:09 -0400

    chore: Add configuration file for ESLint with no overrides

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
