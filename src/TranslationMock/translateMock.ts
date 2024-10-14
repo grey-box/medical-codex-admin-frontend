4a39f10877166a2f90cc31390078f32acbc4f6b4
Kaguyasuo <williamkhorton@gmail.com>
2024-10-14 15:42:30 -0400

    Translation feature finished

--- src/TranslationMock/translateMock.ts
+++ src/TranslationMock/translateMock.ts
@@ -1,24 +1,37 @@
 import translationData from './TranslationTestData.json';
 const translateMock = async (
-    inputSearch: (translation: string) => void,
+    inputSearch: string,
     selectedLangSource: string,
     selectedLangTarget: string,
     setOutputTranslation: (translation: string) => void,
   ): Promise<void> => {
       // Not implemented yet
       console.log(inputSearch)
+      // const response = await fetch('./TranslationTestData.json');
+      // const json = await response.json();
+      setOutputTranslation("");
+      const json = translationData
+      console.log(selectedLangSource)
+      json.results.forEach((result) => {
+          const {en_name, uk_name, ru_name} = result;
+          if (selectedLangSource === 'English' && selectedLangTarget === 'Ukrainian' && inputSearch === en_name) {
+              setOutputTranslation(uk_name);
+          } else if (selectedLangSource === 'English' && selectedLangTarget === 'Russian' && inputSearch === en_name) {
+              setOutputTranslation(ru_name);
+          } else if (selectedLangSource === 'Ukrainian' && selectedLangTarget === 'English' && inputSearch === uk_name) {
+              setOutputTranslation(en_name);
+          } else if (selectedLangSource === 'Ukrainian' && selectedLangTarget === 'Russian' && inputSearch === uk_name) {
+              setOutputTranslation(ru_name);
+          } else if (selectedLangSource === 'Russian' && selectedLangTarget === 'English' && inputSearch === ru_name) {
+              setOutputTranslation(en_name);
+          } else if (selectedLangSource === 'Russian' && selectedLangTarget === 'Ukrainian' && inputSearch === ru_name) {
+              setOutputTranslation(uk_name);
+          }
+      })
+      
   };
   
   export default translateMock;
-// export const parseTranslationData = (data = translationData) => {
-//     const result = {};
-//     data.results.forEach((result) => {
-//         const {en_name, uk_name, ru_name} = result;
-//         result[en_name] = {uk: uk_name, ru: ru_name};
-//         result[uk_name] = {en: en_name, ru: ru_name};
-//         result[ru_name] = {en: en_name, uk: uk_name};
-//     });
-//     return result;
-// };
+  
 
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,6 +35,8 @@ function Navbar(label) {
 
     const style = `
         #navbar {
+            position: fixed;
+            top: 0;
             width: 100%;
             height: 3.5em;
             background-color: grey;
