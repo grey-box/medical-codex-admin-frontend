37db95f9e1ca43e258e004222fad3c7c7cf09326
Matthew Quijano <mq022002@gmail.com>
2024-10-21 19:58:04 -0400

    feat: Adjust handleTranslate to match what the /translate route expects

--- src/utils/handleTranslate/index.ts
+++ src/utils/handleTranslate/index.ts
@@ -5,21 +5,31 @@ const handleTranslate = async (
   NEXT_PUBLIC_API_URL: string | undefined,
 ): Promise<void> => {
   try {
+    const requestBody = {
+      translation_query: {
+        matching_name: selectedMedicine,
+        matching_source: "example_source",
+        matching_uid: 1,
+      },
+      target_language: targetLanguage,
+    };
     const response = await fetch(`${NEXT_PUBLIC_API_URL}/translate/`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        query: selectedMedicine,
-        target_language: targetLanguage,
-      }),
+      body: JSON.stringify(requestBody),
     });
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const dataFromServer = await response.json();
-    setOutputTranslation(dataFromServer.translated_name);
+    const firstResult = dataFromServer.results?.[0];
+    if (firstResult) {
+      setOutputTranslation(firstResult.translated_name);
+    } else {
+      throw new Error("No translation results available.");
+    }
   } catch (error) {
     console.error("Error in handleTranslate function:", error);
   }
