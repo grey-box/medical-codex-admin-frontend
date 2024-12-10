381dee0d6a321a3eb3f8d38f2b61089b4e08b45c
Matthew Quijano <mq022002@gmail.com>
2024-12-10 01:36:00 -0500

    chore: Adjust timeouts or commands and page loads

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
   },
 });
